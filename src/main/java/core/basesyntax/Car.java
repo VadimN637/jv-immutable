d3a30020da148a18701a331c0f0f1f0bcc047ec2
VadimN637 <turnedofff@gmail.com>
2026-04-14 18:21:05 +0300

    12

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -15,17 +15,20 @@ public final class Car {
         this.color = color;
 
         this.wheels = new ArrayList<>();
+
         if (wheels != null) {
-            for (Wheel w : wheels) {
-                if (w != null) {
-                    this.wheels.add(new Wheel(w.getRadius()));
+            for (Wheel wheel : wheels) {
+                if (wheel != null) {
+                    this.wheels.add(new Wheel(wheel.getRadius()));
                 }
             }
         }
 
-        this.engine = engine == null
-                ? null
-                : new Engine(engine.getHorsePower(), engine.getManufacturer());
+        if (engine != null) {
+            this.engine = new Engine(engine.getHorsePower(), engine.getManufacturer());
+        } else {
+            this.engine = null;
+        }
     }
 
     public int getYear() {
@@ -37,35 +40,38 @@ public final class Car {
     }
 
     public Engine getEngine() {
-        return engine == null
-                ? null
-                : new Engine(engine.getHorsePower(), engine.getManufacturer());
+        if (engine == null) {
+            return null;
+        } else {
+            return new Engine(engine.getHorsePower(), engine.getManufacturer());
+        }
     }
 
     public List<Wheel> getWheels() {
         List<Wheel> copy = new ArrayList<>();
-        for (Wheel w : wheels) {
-            copy.add(new Wheel(w.getRadius()));
+
+        for (Wheel wheel : wheels) {
+            copy.add(new Wheel(wheel.getRadius()));
         }
+
         return copy;
     }
 
     public Car changeEngine(Engine engine) {
-        return new Car(year, color, wheels, engine);
+        return new Car(year, color, this.wheels, engine);
     }
 
     public Car changeColor(String newColor) {
-        return new Car(year, newColor, wheels, engine);
+        return new Car(year, newColor, this.wheels, this.engine);
     }
 
     public Car addWheel(Wheel newWheel) {
-        List<Wheel> newWheels = new ArrayList<>();
-        for (Wheel w : wheels) {
-            newWheels.add(new Wheel(w.getRadius()));
-        }
+        List<Wheel> newWheels = new ArrayList<>(this.wheels);
+
         if (newWheel != null) {
             newWheels.add(new Wheel(newWheel.getRadius()));
         }
+
         return new Car(year, color, newWheels, engine);
     }
 
@@ -74,10 +80,13 @@ public final class Car {
         if (this == o) {
             return true;
         }
+
         if (!(o instanceof Car)) {
             return false;
         }
+
         Car car = (Car) o;
+
         return year == car.year
                 && Objects.equals(color, car.color)
                 && Objects.equals(wheels, car.wheels)
